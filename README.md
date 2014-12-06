32596e618132760e5ac6191aaef10410d9b4268a
Frank XU <xuzhen165@gmail.com>
2014-12-06 16:54:26 -0500

    add download method
    revision in specifications
    add field to Message model

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ colock-server
 ## Dependencies
 This app is based on Django framework and GeTui push messaging service. The dependencies are:
   - django
+  - django-sendfile
 ## Note
   - igt_wrappers.py and settings.py files contain keys that are supposed to be kept private so they are not tracked at all,
   the version here is only a template
--- message/views.py
+++ message/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-import django.http
+# import django.http
+from django.http import HttpResponse
+from django.forms import ModelForm
+from message.models import Message
+from message.models import upload_prefix
+from django.views.decorators.csrf import csrf_exempt
+from sendfile import sendfile
+
+class DownloadForm(ModelForm):
+    class Meta:
+        fields = ['message_key']
 
 # Create your views here.
-def send():
+# def send(request):
 
-def push():
-
-def upload():
-
-def download():
+@csrf_exempt
+def download(request):
+    if request.method == "POST":
+        msg_id = int( request.POST["message_id"] )
+        msg_key = request.POST["message_key"]
+        if Message.objects.get(id=msg_id).message_key == msg_key:
+            filepath = ( "%s/%s" % ( upload_prefix, msg_key ) )
+            img_file = open(filepath)
+            # make a response
+            return sendfile(img_file)
+    else:
+        return HttpResponse(status=405)
+    # print request.POST
