5528bd9749382ab945860b20a018d8c16ef836e8
Frank XU <xuzhen165@gmail.com>
2014-12-30 21:21:22 -0500

    fix import path problem

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.append("/home/colock/site/colock-server/message/")
+    sys.path.append("message/")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "colock.settings")
 
     from django.core.management import execute_from_command_line
