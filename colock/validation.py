f3652a5234cfa5581184b27ec39e2986056b3e5a
jdaaph <Chengyu@Chengyus-MacBook-Pro.local>
2015-01-05 22:57:01 -0500

    add validation

--- colock/validation.py
+++ colock/validation.py
@@ -1,10 +1,12 @@
 import json
 from user_manage.models import User
 from colock import utils
+import os
+from settings import BASE_DIR
 __author__ = 'Chengyu'
 
 
-@utils.hook('')
+@utils.hook()
 def is_valid_user(uid, ukey):
     query = User.objects.filter(id=uid)
     if len(query) == 0:
@@ -14,7 +16,8 @@ def is_valid_user(uid, ukey):
     else:
         return False, 'Wrong key'
 
-@utils.hook('')
+
+@utils.hook()
 def is_valid_uid(uid):
     query = User.objects.filter(id=uid)
     if len(query) == 0:
@@ -23,12 +26,12 @@ def is_valid_uid(uid):
         return True, ''
 
 
-@utils.hook('')
+@utils.hook()
 def is_valid_phone_num(reg_num,phone_num):
     return True, ''
 
 
-@utils.hook('')
+@utils.hook()
 def is_valid_dispatch(action, meta, data):
     # return (boolean, string), string is the error information, and if it's valid string will be blank
     try:
@@ -44,13 +47,27 @@ def is_valid_dispatch(action, meta, data):
         return False, 'essential information missing in meta'
 
     tmp_err = is_valid_user(sender_uid, sender_ukey)
-    if tmp_err[0]:
-
-        # validate app_version and action
-
-        # validate arguments needed for the action
-
-        pass
-    else:
+    if not tmp_err[0]:
         return tmp_err
+    disp_lst = json.load(BASE_DIR + 'dispatch-list.json')
+
+    # validate app_version and action
+    if app_version not in disp_lst:
+        return False, 'app version does not exist or no longer supported'
+
+    v_lst = disp_lst[app_version]
+    if action not in v_lst:
+        return False, 'action does not exist or not supported in this version'
+
+    # validate arguments needed for the action
+    arg_lst = v_lst[action]
+    for i in arg_lst:
+        if i not in dict_data:
+            return False, 'Missing argument %s for action %s' % (action, i)
+        for j_validation in arg_lst[i]:
+            bingo, reason = utils.call_hook(j_validation, dict_data[i])
+            if not bingo:
+                return bingo, reason
+            else:
+                return True, ''
 
