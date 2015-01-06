4f1e45f2bdeb6c453dba0e9cc5bc42b6cc981cab
Frank XU <xuzhen165@gmail.com>
2015-01-05 23:06:54 -0500

    dispatch actions

--- colock/dispatch.py
+++ colock/dispatch.py
@@ -1,23 +1,28 @@
 #!/bin/env python2
 # -*- encoding:utf-8 -*-
 
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django import forms
-import json
 import colock.settings
 from importlib import import_module
+from colock import utils
+from django.views.decorators.csrf import csrf_exempt
 
+imported_modules = []
+class dispatchForm(forms.Form):
+    Action = forms.CharField(max_length=32)
+    Meta = forms.CharField()
+    Data = forms.CharField()
+
+for mod in colock.settings.DISPATCH_MANAGED_APPS:
+    imported_modules.append(import_module(mod + ".views"))
+
+@csrf_exempt
 def dispatch(request):
     if request.method == "POST":
-        body = json.loads(request.POST["body"])
-        return call_hook(body["Action"], request)
+        action = request.POST["Action"]
+        return utils.call_hook(action, request.POST["Meta"], request.POST["Data"])
     else:
-        form_content = {
-            'Action': '',
-            'Meta': '',
-            'Data': ''
-        }
         return render_to_response('register.html',
-                    {'uf': forms.Form(form_content)})
+                    {'uf': dispatchForm()})
 
